d2546491290134bfb35234f2512fcf4356b54a46
shazwendy <akothsharon19@gmail.com>
2019-04-05 21:22:03 +0300

    adding my first program file

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def add(a,b):
+	c = a+b
+	print(c)
+
+def subtract(a,b):
+	c = a-b
+	print(c)
+
+
+def division(a,b):
+	c = a/b
+	print(c)
+
+
+def modulus(a,b):
+	c = a%b
+	print(c)
+
+def multiply(a,b):
+	c = a*b
+	print(c)
+
+def exponent(a,b):
+	c = a**b
+	print(c)
+
+def whole number division(a,b):
+	c = a//b
+	print(c)
+
+def percentage(a,b):
+	c = a/b*100
+	print(c)
